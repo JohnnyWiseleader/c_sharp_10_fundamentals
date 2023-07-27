fbb44df51e9b10497507fe59fae09c9506aef83e
Johnny Wiseleader <leadthrustrength@gmail.com>
2023-07-27 13:46:53 -0700

    Add project files.

---
+++ c_sharp_10_fundamentals/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, everyone!");
+Console.WriteLine("Please enter your name:");
+
+var name = Console.ReadLine();
+Console.WriteLine("Hello {0}", name);
